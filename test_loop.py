45bf423eedd78a2312f67d602724332404d3c68b
Jinho Jung <jvisualschool@gmail.com>
2026-02-20 23:27:34 +0900

    Remove unnecessary development scripts and testing files

--- test_loop.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-import time
-while True:
-    try:
-        urllib.request.urlopen("http://192.168.0.111/update?msg=TEST&anim=1", timeout=1)
-        print("Success")
-    except Exception as e:
-        print("Error:", e)
-    time.sleep(2)
